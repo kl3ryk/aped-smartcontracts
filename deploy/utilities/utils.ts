c263d9552d71ce76542d69446ffbea3bd12df8cb
Paweł Łaskarzewski <kl3ryk@gmail.com>
2024-05-01 01:44:35 +0400

    Add files via upload

---
+++ deploy/utilities/utils.ts
@@ -0,0 +1,42 @@
+import chalk from "chalk";
+import { BigNumber } from "ethers";
+import { DeployResult } from "hardhat-deploy/types";
+
+export function getBigNumber(amount: number, decimals: number = 18): BigNumber {
+  return BigNumber.from(amount).mul(BigNumber.from(10).pow(decimals));
+}
+
+export function dim(logMessage: string): void {
+  console.log(chalk.dim(logMessage));
+}
+
+export function cyan(logMessage: string): void {
+  console.log(chalk.cyan(logMessage));
+}
+
+export function yellow(logMessage: string): void {
+  console.log(chalk.yellow(logMessage));
+}
+
+export function green(logMessage: string): void {
+  console.log(chalk.green(logMessage));
+}
+
+export function displayResult(name: string, result: DeployResult): void {
+  if (!result.newlyDeployed) {
+    yellow(`Re-used existing ${name} at ${result.address}`);
+  } else {
+    green(`${name} deployed at ${result.address}`);
+  }
+}
+
+export const chainName = (chainId: number): string => {
+  switch (chainId) {
+    case 8453:
+      return "Base";
+    case 84532:
+      return "Base Sepolia Testnet";
+    default:
+      return "Hardhat";
+  }
+};
